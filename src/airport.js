278423a1d109ac0ca1906b7dff7e924b3a714824
Dan <daniel.bedford56@gmail.com>
2020-11-16 16:56:51 +0000

    Challenge finished

--- src/airport.js
+++ src/airport.js
@@ -1,6 +1,26 @@
 class Airport {
-  
-  planes() {
-    return [];
+
+  constructor(weather = new Weather()) {
+    this._weather = weather;
+    this._hangar = []
   }
+
+  planes() {
+    return this._hangar;
+  }
+
+  clearForLanding(plane) {
+    if (this._weather.isStormy()) {
+      throw new Error('cannot land during storm')
+    }
+    this._hangar.push(plane);
+  }
+
+  clearForTakeOff(plane) {
+    if (this._weather.isStormy()) {
+      throw new Error('cannot takeoff during storm')
+    }
+    this._hangar = [];
+  }
+  
 }
